fe9ca850590ce7ab49e02972f81cbd97cd18d177
Dani <daruedada@gmail.com>
2026-04-14 12:09:14 +0200

    user dani root

--- core/settings.py
+++ core/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'projecte_vi', # Nom de la BBDD
-        'USER': 'root',
+        'USER': 'dani',
         'PASSWORD': 'EwnizEv5',  # De secure_installation
         'HOST': '127.0.0.1',
         'PORT': '3306',
